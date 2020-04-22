6bc600036ed0f191cdf14d6c8d20b182fd3e1555
bcwan <bcwan8@gmail.com>
2020-04-22 10:35:11 -0700

    updated signup component

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -8,10 +8,54 @@ class Signup extends React.Component {
       email: '',
       password: '',
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  //update (type which can be username, email, password)
+  handleInput(type) {
+    return (e) => {
+      this.setState({
+        [type]: e.target.value
+      })
+    }
+  }
+
+  //we want to prevent default because button causes default post which would
+  //rerender and we don't want to
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createNewUser(this.state)
+      .then(() => this.props.history.push('./chirps'));
+  }
+
+  //anytime there's a change, call handleInput
   render() {
     return (
-      <div></div>
+      <div className="session-form">
+        <h2>Sign Up!</h2>
+        <form>
+          <label>Username: 
+            <input type="text" 
+                   value={this.state.username}
+                   onChange={this.handleInput('username')}
+            />
+          </label>
+          <label>Email:
+            <input type="text"
+              value={this.state.email}
+              onChange={this.handleInput('email')}
+            />
+          </label>
+          <label>Password:
+            <input type="password"
+              value={this.state.password}
+              onChange={this.handleInput('password')}
+            />
+          </label>
+          <button onClick={this.handleSubmit}>Sign Up</button>
+        </form>
+      </div>
     );
   }
 };
